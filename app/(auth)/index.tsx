a0c874a04a1df92b0af39e6ff6951829f6aac64e
iyarsius <iyarsius@gmail.com>
2026-04-04 16:30:00 +0200

    Auth page

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,9 +1,43 @@
+import Button from "@/components/Button";
+import { useThemColors } from "@/theme/useThemColors";
+import { componentSpacing, fontStyle } from "@/theme/utils";
+import { router } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Auth() {
+  const colors = useThemColors();
+
   return (
-    <View>
-      <Text>Auth</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 24,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          gap: componentSpacing("small"),
+        }}
+      >
+        <Text style={[fontStyle("heading", "large")]}>Blind</Text>
+        <Text
+          style={[fontStyle("text", "large"), { color: colors.neutral[300] }]}
+        >
+          Crypto made private
+        </Text>
+      </View>
+      <View style={{ flex: 1, justifyContent: "flex-end" }}>
+        <Button
+          onPress={() => {
+            router.push("/(pages)/Home");
+          }}
+        >
+          Create Wallet
+        </Button>
+      </View>
     </View>
   );
 }
